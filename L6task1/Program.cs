3aed06716bd3d0d083951363734d62b7dbf1f8da
GeorgiiShamamian <123135299+GeorgiiShamamian@users.noreply.github.com>
2023-02-07 18:44:50 +0300

    Add files via upload

---
+++ L6task1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int [] CreateArray (string message)
+{
+    System.Console.WriteLine(message + " > ");
+    int sizeArray = Convert.ToInt32(Console.ReadLine());
+    int [] array = new int[sizeArray];
+    return array;
+}
+
+int[] CompletionArray(int [] baseArray, string message)
+{
+    for (int i = 0; i < baseArray.Length; i++)
+    {
+        System.Console.WriteLine(message + $" > ");
+        baseArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return baseArray;
+}
+
+void OutputElements(int[] baseArray)
+{
+    System.Console.WriteLine("Введенные числа:");
+    for (int i = 0; i < baseArray.Length; i++)
+    {
+        System.Console.WriteLine($"{baseArray[i]}");
+    }
+}
+
+void CheckPositiveNum (int[] baseArray)
+{
+    int count = 0;
+    for (int i = 0; i < baseArray.Length; i++)
+    {
+        if (baseArray[i] > 0)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine($"Количество чисел больше нуля > {count}");
+}
+
+int[] baseArray = CreateArray ("Введите количество чисел для ввода");
+string message = $"Введите число";
+CompletionArray(baseArray, message);
+OutputElements(baseArray);
+CheckNum(baseArray);
---
+++ L6task2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+const int K1 = 0;
+const int B1 = 1;
+const int K2 = 2;
+const int B2 = 3;
+
+void InputData(double[] parametr, string message)
+{
+    System.Console.WriteLine("Введите параметр K1 > ");
+    System.Console.WriteLine("Введите параметр B1 > ");
+    System.Console.WriteLine("Введите параметр K2 > ");
+    System.Console.WriteLine("Введите параметр B2 > ");
+    for (int i = 0; i <= parametr.Length - 1; i++)
+    {
+        parametr[i] = Convert.ToDouble(Console.ReadLine());
+    }
+}
+
+double FindX (double[] parametr)
+{
+    double x = (parametr[B2] - parametr[B1]) / (parametr[K1] - parametr[K2]);
+    return x;
+}
+
+double FindY (double[] parametr)
+{
+    double x = FindX(parametr);
+    double y = parametr[K1] * x + parametr[B1];
+    return y;
+}
+
+double[] parametr = new double[4];
+InputData (parametr, "Введите расчетные параметры");
+double resultX = FindX(parametr);
+double resultY = FindY(parametr);
+System.Console.WriteLine($"Координаты пересечения двух прямых будут в точке > {resultX:F1}, {resultY:F1}");
