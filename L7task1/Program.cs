c3aec0106810cc0d60f276ff6af147a3943c523a
GeorgiiShamamian <123135299+GeorgiiShamamian@users.noreply.github.com>
2023-02-10 17:37:43 +0300

    Add files via upload

---
+++ L7task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] CreateArray()
+{
+    double[,] array = new double[5, 5];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(rnd.Next(-100, 100)) / 10;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + "\t");
+        }
+    }
+    System.Console.WriteLine();
+}
+
+double[,] resultArray = CreateArray();
+PrintArray(resultArray);
---
+++ L7task2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int[,] CreateArray(int m = 3, int n = 4)
+{
+    int[,] array = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + "\t");
+        }
+    }
+    System.Console.WriteLine();
+}
+
+int InputInt(string message)
+{
+    System.Console.Write(message + " > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] resultArray = CreateArray();
+PrintArray(resultArray);
+int n = InputInt("Введите номер строки");
+int m = InputInt("Введите номер столбца");
+if (n < 1 || m < 1 || n > resultArray.GetLength(0) || m > resultArray.GetLength(1))
+{
+    System.Console.WriteLine("Такого элемента нет в массиве");
+}
+else
+{
+    System.Console.WriteLine($"Элемент {m} строки и {n} столбца равен {resultArray[n - 1, m -1]}");
+}
