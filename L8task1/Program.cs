4baac4333e78a7871a6e70b6e223d6e0876cad3c
GeorgiiShamamian <123135299+GeorgiiShamamian@users.noreply.github.com>
2023-02-14 05:42:56 +0300

    Add files via upload

---
+++ L8task1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = Math.Round(new Random().NextDouble() * (maxValue - minValue) + minValue, 2);
+        }
+    }
+    return result;
+}
+
+void WriteArrayToConsole(double[,] doubleArray)
+{
+    for (int i = 0; i < doubleArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < doubleArray.GetLength(1); j++)
+        {
+            System.Console.Write($"{doubleArray[i, j]}\t");
+        }
+    }
+}
+
+double[,] SortRows(double[,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+        for (int i = 0; i < matrix.GetLength(1); i++)
+            for (int j = matrix.GetLength(1) - 1; j > i; j--)
+                if (matrix[rows, j] > matrix[rows, j-1])
+                {
+                    double tmp = matrix[rows, j];
+                    matrix[rows, j] = matrix [rows, j-1];
+                    matrix[rows, j-1] = tmp;
+                }
+    return matrix;
+}
+
+int rows = 3;
+int columns = 5;
+double[,] d_array = GetArray(rows, columns, -10, 10);
+System.Console.WriteLine("Начальный массив > ");
+WriteArrayToConsole(d_array);
+System.Console.WriteLine("\nУпорядоченный массив > ");
+WriteArrayToConsole(SortRows(d_array));
---
+++ L8task3/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = Math.Round(new Random().NextDouble() * (maxValue - minValue) + minValue, 2);
+        }
+    }
+    return result;
+}
+
+void WriteArrayToConsole(double[,] doubleArray)
+{
+    for (int i = 0; i < doubleArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < doubleArray.GetLength(1); j++)
+        {
+            System.Console.Write($"{doubleArray[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[,] GetMultArray(double[,] firstArray, double[,] secondArray)
+{
+    double[,] resultArray = new double [firstArray.GetLength(0), secondArray.GetLength(1)];
+    for (int i = 0; i < firstArray.GetLength(0); i++)
+        for (int j = 0; j < secondArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < firstArray.GetLength(1); k++)
+            {
+                resultArray [i, j] += firstArray [i, k] * secondArray [k, j];
+            }
+            resultArray [i, j] = Math.Round(resultArray[i, j], 2);
+        }
+    return resultArray;
+}
+
+int rows1 = 3;
+int columns1 = 5;
+int rows2 = columns1;
+int columns2 = 3;
+double[,] Array1 = GetArray(rows1, columns1, -10, 10);
+double[,] Array2 = GetArray(rows2, columns2, -10, 10);
+double[,] result = GetMultArray(Array1, Array2);
+System.Console.WriteLine("Массив 1 > ");
+WriteArrayToConsole(Array1);
+System.Console.WriteLine($"\nМассив 2 > ");
+WriteArrayToConsole(Array2);
+System.Console.WriteLine($"\nПроизведение массивов > ");
+WriteArrayToConsole(result);
